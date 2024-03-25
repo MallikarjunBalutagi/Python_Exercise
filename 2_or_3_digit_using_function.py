7fbacd0eaeed3356945e31a9b538541b9d851b77
MallikarjunBalutagi <162403799+MallikarjunBalutagi@users.noreply.github.com>
2024-03-25 14:42:36 +0530

    Update 2_or_3_digit_using_function.py

--- 2_or_3_digit_using_function.py
+++ 2_or_3_digit_using_function.py
@@ -3,6 +3,8 @@ def perform_check(number):
         print("two digit number")
     elif 100 <= number <=999:
         print("three digit number")
+    else:
+        print(number)
 def get_number():
     number = int(input("enter the number: "))
     return number
