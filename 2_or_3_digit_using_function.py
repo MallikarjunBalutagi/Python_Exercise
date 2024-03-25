1fdc5c935fe849d11178d80adc11f7d72b9ef557
MallikarjunBalutagi <162403799+MallikarjunBalutagi@users.noreply.github.com>
2024-03-25 14:41:41 +0530

    Add files via upload
    Take 2 numbers from the user.Print which number is a 2 digit number,and which number is a 3 digit number. If it is neither, then print the number as it is

---
+++ 2_or_3_digit_using_function.py
@@ -0,0 +1,14 @@
+def perform_check(number):
+    if 10 <= number <= 99:
+        print("two digit number")
+    elif 100 <= number <=999:
+        print("three digit number")
+def get_number():
+    number = int(input("enter the number: "))
+    return number
+    
+num1 = get_number()
+num2 = get_number()
+
+perform_check(num1)
+perform_check(num2)
